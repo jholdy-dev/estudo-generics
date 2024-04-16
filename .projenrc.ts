05593889de085f5db7ffe105dc0b2a69c53aa16d
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-04-16 16:15:58 -0300

    chore: start typescript

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,21 @@
-import { typescript } from 'projen';
+import { JsonFile, typescript } from 'projen'
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'estudo-generics',
   projenrcTs: true,
   prettier: true,
-});
+  eslint: true,
+})
 
-project.synth();
+project.tryRemoveFile('.prettierrc.json')
 
+new JsonFile(project, '.prettierrc.json', {
+  obj: {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    endOfLine: 'auto',
+  },
+})
+
+project.synth()
